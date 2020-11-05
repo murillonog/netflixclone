c99c85a82ad95b54df0f206b7b8da16fd52f68ab
Murillo <murillo.nogueira01@outlook.com>
2020-11-04 21:26:08 -0300

    Criado efeito da rolagem de lista

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MovieRow.css';
+import {FiChevronLeft} from "react-icons/fi";
+import {FiChevronRight} from "react-icons/fi";
+
 
 export default ({title, items}) => {
+    const [scrollX, setScrollX] = useState(-400);
+
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth/2);;
+        debugger;
+        if(x > 0) {
+            x = 0;
+        }
+
+        setScrollX(x);
+    }
+
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth/2);
+        let listW = items.results.length * 150;
+        let result = window.innerWidth - listW;
+        debugger;
+        if((window.innerWidth - listW) > x) {
+            x = window.innerWidth - listW - 60;
+        }
+
+        setScrollX(x);
+    }
+
     return (
         <div className="movieRow">
             <h2>{title}</h2>
+
+            <div className="movieRow--left" onClick={handleLeftArrow}>
+                <FiChevronLeft style={{fontSize:50}}/>
+            </div>
+            <div className="movieRow--right" onClick={handleRightArrow}>
+                <FiChevronRight style={{fontSize:50}}/>
+            </div>
             <div className="movieRow--listarea">
-                <div className="movieRow--list">
+                <div className="movieRow--list" style={{
+                    marginLeft: scrollX,
+                    width: items.results.length * 150
+                    }}>
                     {items.results.length > 0 && items.results.map((item, key) => (
                         <div className="movieRow--item" key={key}>
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
